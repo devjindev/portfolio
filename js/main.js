ab84f3530d49a0144be8487b93d5f39ca93cbb60
HYJ <hyjinny00@gmail.com>
2021-02-02 22:46:00 +0900

    CSS: nav animation

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Nav scroll
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
